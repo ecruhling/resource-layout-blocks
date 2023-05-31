d7e420bbc03a6dce9858097aebb6ea2ba3c74a1f
ecruhling <ecruhling@gmail.com>
2023-05-31 16:59:50 -0500

    begin switch from dynamic to static for container block
    Signed-off-by: ecruhling <erik@resourceatlanta.com>

--- build/container.asset.php
+++ build/container.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fdcfe3374ddcd133e7f8');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '117ffc2b478aacf9340d');
--- includes/block-editor/blocks/container/save.js
+++ includes/block-editor/blocks/container/save.js
@@ -1,18 +1,40 @@
 /**
- * WordPress dependencies.
+ * WordPress dependencies
  */
-import {InnerBlocks} from '@wordpress/block-editor';
+import {useInnerBlocksProps, useBlockProps} from '@wordpress/block-editor';
+
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import {omit} from 'lodash';
 
 /**
  * The Block save function.
  *
- * Even though this is a dynamic block, it needs the save function to save any InnerBlocks contained within.
- *
  * @returns {JSX.Element}
  * @constructor
  */
-export default function save () {
-	return (
-		<InnerBlocks.Content />
+export default function save ({
+																attributes,
+															}) {
+
+	const {
+		isFluid,
+		tagName: TagName = 'div',
+	} = attributes;
+
+	const classNameAttributes = omit(attributes, ['anchor', 'isFluid', 'tagName', 'className']);
+
+	const className = classnames(
+		{
+			'container': !isFluid,
+			'container-fluid': isFluid,
+		},
+		Object.values(classNameAttributes),
 	);
+
+	// console.log(classes);
+
+	return <TagName {...useBlockProps.save({className})} />;
 }
