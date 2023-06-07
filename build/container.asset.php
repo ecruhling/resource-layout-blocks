daaf913d3030c03231a3e22ee4506c34f4f49000
ecruhling <ecruhling@gmail.com>
2023-06-07 16:55:39 -0500

    CSS to make sidebar a little wider
    Signed-off-by: ecruhling <erik@resourceatlanta.com>

--- build/container.asset.php
+++ build/container.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ccaa0eea6bc5f14c410e');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2f057bba1d5a02b09a23');
