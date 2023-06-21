86571b254e841c8521eae6b40a9af54307b59862
ecruhling <ecruhling@gmail.com>
2023-06-21 12:51:20 -0500

    change <div> icon
    Signed-off-by: ecruhling <erik@resourceatlanta.com>

--- build/row.asset.php
+++ build/row.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '95ed4815f687b5e6ec31');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ae7955d759082d06daba');
