b14c7831abbe22811b4c83035719aa594b1d231c
ecruhling <ecruhling@gmail.com>
2023-06-22 14:10:33 -0500

    <div> edit
    Signed-off-by: ecruhling <erik@resourceatlanta.com>

--- build/container.asset.php
+++ build/container.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '80fb9697b234a6cccdd7');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9cd8d01db34bddb7aa92');
--- includes/block-editor/blocks/div/edit.js
+++ includes/block-editor/blocks/div/edit.js
@@ -2,11 +2,22 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { InnerBlocks } from '@wordpress/block-editor';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import { omit } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import DisplayControl from '../../block-controls/displayControl';
+import PaddingControl from '../../block-controls/paddingControl';
+import MarginControl from '../../block-controls/marginControl';
+import AlignControl from '../../block-controls/alignControl';
+import FlexControl from '../../block-controls/flexControl';
 import { convertStylesStringToObject } from '../../../lib/convertStylesStringToObject';
 
 /**
@@ -22,8 +33,136 @@ import './editor.scss';
  * @returns {JSX.Element}
  * @constructor
  */
-
 export default function edit( { attributes, setAttributes } ) {
+	const {
+		className,
+		inlineStyles,
+		baseDisplay,
+		basePaddingTop,
+		basePaddingRight,
+		basePaddingBottom,
+		basePaddingLeft,
+		baseMarginTop,
+		baseMarginRight,
+		baseMarginBottom,
+		baseMarginLeft,
+		baseAlignText,
+		baseFlexDirection,
+		baseFillGrowShrink,
+		baseWrap,
+		baseOrder,
+		baseAlignContent,
+		baseAlignItems,
+		baseAlignSelf,
+		baseJustifyContent,
+		smDisplay,
+		smPaddingTop,
+		smPaddingRight,
+		smPaddingBottom,
+		smPaddingLeft,
+		smMarginTop,
+		smMarginRight,
+		smMarginBottom,
+		smMarginLeft,
+		smAlignText,
+		smFlexDirection,
+		smFillGrowShrink,
+		smWrap,
+		smOrder,
+		smAlignContent,
+		smAlignItems,
+		smAlignSelf,
+		smJustifyContent,
+		mdDisplay,
+		mdPaddingTop,
+		mdPaddingRight,
+		mdPaddingBottom,
+		mdPaddingLeft,
+		mdMarginTop,
+		mdMarginRight,
+		mdMarginBottom,
+		mdMarginLeft,
+		mdAlignText,
+		mdFlexDirection,
+		mdFillGrowShrink,
+		mdWrap,
+		mdOrder,
+		mdAlignContent,
+		mdAlignItems,
+		mdAlignSelf,
+		mdJustifyContent,
+		lgDisplay,
+		lgPaddingTop,
+		lgPaddingRight,
+		lgPaddingBottom,
+		lgPaddingLeft,
+		lgMarginTop,
+		lgMarginRight,
+		lgMarginBottom,
+		lgMarginLeft,
+		lgAlignText,
+		lgFlexDirection,
+		lgFillGrowShrink,
+		lgWrap,
+		lgOrder,
+		lgAlignContent,
+		lgAlignItems,
+		lgAlignSelf,
+		lgJustifyContent,
+		xlDisplay,
+		xlPaddingTop,
+		xlPaddingRight,
+		xlPaddingBottom,
+		xlPaddingLeft,
+		xlMarginTop,
+		xlMarginRight,
+		xlMarginBottom,
+		xlMarginLeft,
+		xlAlignText,
+		xlFlexDirection,
+		xlFillGrowShrink,
+		xlWrap,
+		xlOrder,
+		xlAlignContent,
+		xlAlignItems,
+		xlAlignSelf,
+		xlJustifyContent,
+		xxlDisplay,
+		xxlPaddingTop,
+		xxlPaddingRight,
+		xxlPaddingBottom,
+		xxlPaddingLeft,
+		xxlMarginTop,
+		xxlMarginRight,
+		xxlMarginBottom,
+		xxlMarginLeft,
+		xxlAlignText,
+		xxlFlexDirection,
+		xxlFillGrowShrink,
+		xxlWrap,
+		xxlOrder,
+		xxlAlignContent,
+		xxlAlignItems,
+		xxlAlignSelf,
+		xxlJustifyContent,
+	} = attributes;
+
+	const classNameAttributes = omit( attributes, [
+		'anchor',
+		'inlineStyles',
+		'className',
+	] );
+
+	const classes = classnames(
+		Object.values( classNameAttributes ),
+		className
+	);
+
+	const blockProps = useBlockProps( {
+		className: classes,
+		style: convertStylesStringToObject( inlineStyles ),
+	} );
+
 	return (
 		<div { ...blockProps }>
 			<InnerBlocks placeholder={ __( 'Insert Blocks', 'resource' ) } />
